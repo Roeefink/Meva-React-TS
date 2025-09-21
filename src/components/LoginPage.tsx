fddb7152132abcd4fbcb4befda9855dfd436f900
Roeefink <roeefink@icloud.com>
2025-09-21 14:50:22 +0300

    Fix for Firebase

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,10 +1,7 @@
 import React, { useState } from "react";
 import { Eye, EyeOff, Mail, Lock, LogIn } from "lucide-react";
-import { auth, db } from "../config/Firebase.ts";
-import {
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-} from "firebase/auth";
+import { auth } from "../config/Firebase.ts";
+import { signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { Link } from "react-router-dom";
 
 interface FormData {
